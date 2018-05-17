042a555975fee0473487dee8b5ccd0a3a5095004
ruben <ruben.valdez87@gmail.com>
2018-05-17 17:55:55 -0500

    last modification for today

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Sortable from 'vue-sortable/vue-sortable.js'
 
 
 Vue.config.productionTip = false
-Vue.use(Sortable)
 
 new Vue({
   render: h => h(App)
